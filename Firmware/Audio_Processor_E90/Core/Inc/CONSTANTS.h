9c53a69852266db03f38a8e5fad72be2e1ba314d
Skyler Cornell <skylerhcornell@gmail.com>
2021-01-22 14:51:00 -0500

    revisions to audio framework

---
+++ Firmware/Audio_Processor_E90/Core/Inc/CONSTANTS.h
@@ -0,0 +1,19 @@
+/*
+ * CONSTANTS.h
+ *
+ *  Created on: Jan 22, 2021
+ *      Author: skylercornell
+ */
+
+#ifndef INC_CONSTANTS_H_
+#define INC_CONSTANTS_H_
+
+
+
+#endif /* INC_CONSTANTS_H_ */
+
+#define SAMPLE_RATE 48000
+#define SAMPLE_RATE_ACTUAL 46875
+#define BIT_DEPTH 24
+
+#define PI (float)3.14159265359
---
+++ Firmware/Audio_Processor_E90/Core/Src/audio_process.c
@@ -0,0 +1,37 @@
+/*
+ * audio_process.c
+ *
+ *  Created on: Jan 22, 2020
+ *      Author: skylercornell
+ */
+
+#include "audio_process.h"
+#include <math.h>
+
+/*
+ * Declare global variables and structures units here
+ */
+
+
+/*
+ * Executes once before entering program loop
+ */
+void process_init()
+{
+
+
+}
+
+/*
+ * Main DSP routine, CPU enters function once per sample period
+ * samples are right justified 24 bit signed
+ */
+void process(int *left_in, int *right_in, int *left_out, int *right_out)
+{
+
+
+  *right_out = right_in;
+  *left_out = left_in;
+
+}
+
