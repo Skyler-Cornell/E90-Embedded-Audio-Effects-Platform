7343d1c049567721642dac035b8211debbc3e062
Skyler Cornell <skylerhcornell@gmail.com>
2021-01-22 14:54:39 -0500

    revisions to audio framework

--- Firmware/Audio_Processor_E90/Core/Src/audio_process.c
+++ Firmware/Audio_Processor_E90/Core/Src/audio_process.c
@@ -30,8 +30,8 @@ void process(int *left_in, int *right_in, int *left_out, int *right_out)
 {
 
 
-  *right_out = right_in;
-  *left_out = left_in;
+  *right_out = *right_in;
+  *left_out = *left_in;
 
 }
 
